1537fb94f53953bbbcc1ba510f82507127c3636c
bibekp <bibekp@txstate.edu>
2024-12-27 17:51:46 -0600

    Project Completed

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    state = request.args.get('state', 'NA')
+    country = request.args.get('country', 'US')
+    unit = request.args.get('unit', 'imperial')  # Default to Fahrenheit if no unit is provided
+
+    if not bool(city.strip()):
+        city = "Lynnwood"  # Default city if input is empty
+
+    try:
+        weather_data = get_current_weather(city, unit, state, country)
+
+        # Check if the response code is 200 (OK)
+        if weather_data['cod'] != 200:
+            return render_template('city-not-found.html', city=city, unit=unit, state=state, country=country)
+        
+        return render_template(
+            'weather.html',
+            title=weather_data["name"],
+            status=weather_data["weather"][0]["description"].capitalize(),
+            temp=f"{weather_data['main']['temp']:.1f}",
+            feels_like=f"{weather_data['main']['feels_like']:.1f}",
+            unit=unit,
+            maxtemp = f"{weather_data['main']['temp_max']:.1f}",
+            mintemp = f"{weather_data['main']['temp_min']:.1f}"
+        )
+
+    except requests.exceptions.RequestException as req_err:
+        return render_template('error.html', message="Unable to connect to the weather service. Please check your internet connection.")
+    except Exception as e:
+        return render_template('error.html', message=f"An error occurred.")
+
+
+if __name__ == "__main__":
+    app.config['DEBUG'] = True
+    app.config['PORT'] = 8000
+    serve(app, host="0.0.0.0", port=app.config['PORT'])
---
+++ weather.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import requests
+import os
+from pprint import pprint
+
+load_dotenv()
+
+def get_current_weather(city="Lynnwood", unit="imperial", state='WA', country='US'):
+    """
+    Get current weather data from OpenWeatherMap API.
+    
+    :param city: City name for weather data.
+    :param unit: Unit for temperature ('imperial' for Fahrenheit, 'metric' for Celsius).
+    :return: Weather data in JSON format.
+    """
+    if state == 'NA':
+        
+
+        request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('API_KEY')}&units={unit}"
+    else:
+        request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={os.getenv('API_KEY')}&units={unit}"
+
+    weather_data = requests.get(request_url).json()
+    
+    return weather_data
+
+
+if __name__ == "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input("\nPlease enter a city name: \n")
+    state = input("\nPlease enter a state name: \n")
+    unit = input("Choose temperature unit (C for Celsius, F for Fahrenheit): ").strip().lower()
+    country = input("Country name: ").strip().lower()
+
+    if not bool(city.strip()):
+        city = "Lynnwood"
+    
+    if unit not in ['c', 'f']:
+        unit = 'f'  # Default to Fahrenheit if invalid input
+
+    weather_data = get_current_weather(city, 'metric' if unit == 'c' else 'imperial', state, country)
+
+    print("\nWeather Data:")
+    pprint(weather_data)
